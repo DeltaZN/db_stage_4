1368bb493496150426ce4cfd9ad3b92ac5457c2f
Georgii Savin <gsavin@devexperts.com>
2020-11-24 23:04:02 +0300

    init

---
+++ src/main/kotlin/ru/itmo/coffee/store/controller/TestController.kt
@@ -0,0 +1,12 @@
+package ru.itmo.coffee.store.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class TestController {
+    @GetMapping("/")
+    fun test(): String {
+        return "test"
+    }
+}
---
+++ src/main/kotlin/ru/itmo/coffee/store/entities/Customer.kt
@@ -0,0 +1,3 @@
+package ru.itmo.coffee.store.entities
+
+class Customer(val id: Long, val firstName: String, val lastName: String)
